91aecd870a2af8ded8cb03f7c52a42e0072add99
leveaul <35447922+leveaul@users.noreply.github.com>
2026-05-27 15:59:12 +0200

    sql: add GTWCE 3H Monza round 2 (29-31 May)

---
+++ sql/pending/gtwce_monza_20260527_135911.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO races (series_id, name, circuit, country, date_start, date_end, round, type, status)
+VALUES ('GTWCE', '3H Monza', 'Autodromo Nazionale Monza', 'Italie', '2026-05-30', '2026-05-31', 2, 'race', 'upcoming');
